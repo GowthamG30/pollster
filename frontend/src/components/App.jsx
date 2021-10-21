a92c45e0c439ea85a08d3a6a06131ec285b3d941
Gowtham Gorrepati <gowtham.gorrepati30@gmail.com>
2021-10-21 16:17:10 +0530

    Add components

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import axios from "axios";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
