b5aa3081480f83b235dfa5484539af3547650c3e
Gowtham Gorrepati <gowtham.gorrepati30@gmail.com>
2021-10-21 15:39:31 +0530

    Remove unnecessary files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require("path")
+const cors = require('cors');
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'frontend\\build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'frontend\\build','index.html'));
+});
+
+app.get('/polls', (req, res) => {
+  res.redirect('/');
+});
+
+server.listen(port, () => {
+  console.log('listening on ' + port);
+});
