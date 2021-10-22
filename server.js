ba2d4c0768d21987521b6714df7fd32ae9af0536
Gowtham Gorrepati <gowtham.gorrepati30@gmail.com>
2021-10-22 13:21:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,22 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'frontend\\build')));
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname,'frontend\\build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'frontend\\build','index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname,'frontend\\build','index.html'));
+// });
 
 // app.get('/polls', (req, res) => {
 //   console.log("yeayy");
 //   // res.sendFile(path.join(__dirname,'frontend\\build','index.html'));
 // });
 
-server.listen(port, () => {
+// server.listen(port, () => {
+//   console.log('listening on ' + port);
+// });
+
+app.listen(port, () => {
   console.log('listening on ' + port);
 });
