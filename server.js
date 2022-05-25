33bf89010000d89e0d78240ae99a2f362dcef9ae
Gowtham Gorrepati <gowtham.gorrepati30@gmail.com>
2022-05-25 23:47:26 +0530

    Prepare for deployment - 1
    Added build code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { Poll, User } = require("./database.js");
+const path = require("path");
 const port = process.env.PORT || 5000;
 const saltRounds = parseInt(process.env.SALT_ROUNDS) || 10;
 require("dotenv").config();
@@ -10,7 +11,12 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
 
 const generateAccessToken = (data) => {
 	const token = jwt.sign(data, process.env.JWT_SECRET_KEY, {expiresIn: process.env.TOKEN_LIFE});
